05e7fbb58f0725fe3cf0711da52245ffb729e260
Mortis-Web <memaraaa123@gmail.com>
2024-10-26 16:01:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,5 +42,28 @@ function startCount(el) {
     }
   }, 2000 / goal);
 }
-// ==============VIDEO BACKGROUND REMOVER================
+// ==============DIV SWITCHER================
+let switchers = document.querySelectorAll(".oth");
+let main = document.getElementById("main");
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+switchers.forEach(function (switcher) {
+  switcher.addEventListener("click", function (e){
+    if (e.target.id === "one"){
+      main.style.display = "none";
+      first.style.display = "flex";
+      second.style.display = "none";
+    }
+    if (e.target.id === "two"){
+      main.style.display = "none";
+      second.style.display = "flex";
+      first.style.display = "none";
+    }
+    if (e.target.id === "three"){
+      main.style.display = "flex";
+      second.style.display = "none";
+      first.style.display = "none";
+    }
+  });
+});
 
